59b9e44c3d8527328fe7528bc25b3fcb7983fa3a
Kamil Skowron <emanuel1989@gmail.com>
2021-09-17 17:13:13 +0100

    Lazy user service example

--- lib/fun_ex/v6/user_service.ex
+++ lib/fun_ex/v6/user_service.ex
@@ -2,7 +2,7 @@ defmodule FunEx.V6.UserService do
   require Logger
 
   def allowed?(email, read_fn \\ &File.read/1) do
-    read_fn.("list.json")
+    task(fn () -> read_fn.("list.json") end)
     |> chain(&Jason.decode/1)
     |> fapply(&check_email(&1, email))
     |> fold(& &1, fn error ->
@@ -19,21 +19,61 @@ defmodule FunEx.V6.UserService do
     |> Enum.any?(& &1 == email)
   end
 
-  def map({:ok, data}, iteration_fn) do
-    {:ok, Enum.map(data, iteration_fn)}
+  def task(function) do
+    fn () ->
+      function.()
+    end
   end
 
-  def map({:error, error}, _iteration_fn), do: {:error, error}
-
-  def fapply({:ok, data}, function) do
-    {:ok, function.(data)}
+  def chain(acc, function) do
+    fn () ->
+      case acc.() do
+        {:ok, data} -> function.(data)
+        {:error, error} -> {:error, error}
+      end
+    end
   end
 
-  def fapply({:error, error}, _function), do: {:error, error}
+  # def chain({:ok, data}, function), do: function.(data)
+  # def chain({:error, error}, _function), do: {:error, error}
 
-  def chain({:ok, data}, function), do: function.(data)
-  def chain({:error, error}, _function), do: {:error, error}
+  def fapply(acc, function) do
+    fn () ->
+      case acc.() do
+        {:ok, data} -> {:ok, function.(data)}
+        {:error, error} -> {:error, error}
+      end
+    end
+  end
 
-  def fold({:ok, data}, success_fn, _error_fn), do: success_fn.(data)
-  def fold({:error, error}, _success_fn, error_fn), do: error_fn.(error)
+  # def fapply({:ok, data}, function) do
+  #   {:ok, function.(data)}
+  # end
+  # def fapply({:error, error}, _function), do: {:error, error}
+
+  def map(acc, iteration_fn) do
+    fn () ->
+      case acc.() do
+        {:ok, data} -> Enum.map(data, iteration_fn)
+        {:error, error} -> {:error, error}
+      end
+    end
+  end
+
+  # def map({:ok, data}, iteration_fn) do
+  #   {:ok, Enum.map(data, iteration_fn)}
+  # end
+  # def map({:error, error}, _iteration_fn), do: {:error, error}
+
+  def fold(acc, success_fn, error_fn) do
+    fn () ->
+      case acc.() do
+        {:ok, data} -> success_fn.(data)
+        {:error, error} -> error_fn.(error)
+      end
+    end
+  end
+
+  # def fold({:ok, data}, success_fn, _error_fn), do: success_fn.(data)
+  # def fold({:error, error}, _success_fn, error_fn), do: error_fn.(error)
 end
